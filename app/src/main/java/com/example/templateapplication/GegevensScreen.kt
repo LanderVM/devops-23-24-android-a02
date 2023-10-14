cb81a600e1ac48db6f39df476e9d8e4a39292a3d
SanderGeuens <sander.geuens@student.hogent.be>
2023-10-14 23:09:46 +0200

    toevoegen scroll functionaliteit gegevens scherm

--- app/src/main/java/com/example/templateapplication/GegevensScreen.kt
+++ app/src/main/java/com/example/templateapplication/GegevensScreen.kt
@@ -7,7 +7,9 @@ import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material3.Button
@@ -34,8 +36,10 @@ import androidx.compose.ui.unit.sp
 @Composable
 fun GegevensScreen (modifier: Modifier = Modifier) {
 
+    val scrollState = rememberScrollState()
+
     Column (
-        modifier = Modifier.fillMaxWidth(),
+        modifier = Modifier.fillMaxWidth().verticalScroll(state = scrollState),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Spacer(modifier = Modifier.height(20.dp))
