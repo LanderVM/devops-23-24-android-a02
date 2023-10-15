e2ec26a8f494ad59fb3758096f4e51b8f665e822
SanderGeuens <sander.geuens@student.hogent.be>
2023-10-15 18:43:20 +0200

    toevoegen van uurvelden datumscherm

--- app/src/main/java/com/example/templateapplication/DatumScreen.kt
+++ app/src/main/java/com/example/templateapplication/DatumScreen.kt
@@ -1,17 +1,23 @@
 package com.example.templateapplication
 
 import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import android.widget.DatePicker
+import android.widget.TimePicker
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
@@ -29,8 +35,10 @@ import java.util.Date
 @Composable
 fun DatumScreen (modifier: Modifier = Modifier) {
 
-    var startDate = remember { mutableStateOf("") }
-    var endDate = remember { mutableStateOf("") }
+    var startDatum = remember { mutableStateOf("") }
+    var eindDatum = remember { mutableStateOf("") }
+    var startUur = remember { mutableStateOf("") }
+    var eindUur = remember { mutableStateOf("") }
 
     Column(
         modifier = Modifier.fillMaxWidth(),
@@ -55,86 +63,82 @@ fun DatumScreen (modifier: Modifier = Modifier) {
                 .height(17.dp)
         )
         Spacer(modifier = Modifier.height(20.dp))
-        DatumPicker(startDate,endDate)
+        Text(
+            text = "Datum",
+            textAlign = TextAlign.Center,
+            fontSize = 20.sp
+        )
+        Spacer(modifier= Modifier.height(40.dp))
+        DatumPicker(datum = startDatum, naamDatum = "start datum")
+        Spacer(modifier = Modifier.height(10.dp))
+        TijdPicker(label = "begin uur", value = startUur.value, onChange ={startUur.value=it} )
+        Spacer(modifier= Modifier.height(50.dp))
+        DatumPicker(datum = eindDatum, naamDatum = "eind datum")
+        Spacer(modifier = Modifier.height(10.dp))
+        TijdPicker(label = "eind uur", value = startUur.value, onChange ={startUur.value=it} )
+        Spacer(modifier= Modifier.height(30.dp))
     }
 }
 
 @Composable
-fun DatumPicker(beginDatum:MutableState<String>,eindDatum:MutableState<String>) {
-
-    // Fetching the Local Context
+fun DatumPicker (datum:MutableState<String>,naamDatum:String) {
     val mContext = LocalContext.current
 
-    // Declaring integer values
-    // for year, month and day
-    val startYear: Int
-    val startMonth: Int
-    val startDay: Int
+    val mYear: Int
+    val mMonth: Int
+    val mDay: Int
 
-    val endYear: Int
-    val endMonth: Int
-    val endDay: Int
-
-    // Initializing a Calendar
     val mCalendar = Calendar.getInstance()
 
-    // Fetching current year, month and day
-    startYear = mCalendar.get(Calendar.YEAR)
-    startMonth = mCalendar.get(Calendar.MONTH)
-    startDay = mCalendar.get(Calendar.DAY_OF_MONTH)
-
-    endYear = mCalendar.get(Calendar.YEAR)
-    endMonth = mCalendar.get(Calendar.MONTH)
-    endDay = mCalendar.get(Calendar.DAY_OF_MONTH)
+    mYear = mCalendar.get(Calendar.YEAR)
+    mMonth = mCalendar.get(Calendar.MONTH)
+    mDay = mCalendar.get(Calendar.DAY_OF_MONTH)
 
     mCalendar.time = Date()
 
-    // Declaring a string value to
-    // store date in string format
-
-    // Declaring DatePickerDialog and setting
-    // initial values as current values (present year, month and day)
-    val startDatePickerDialog = DatePickerDialog(
+    val mDatePickerDialog = DatePickerDialog(
         mContext,
         { _: DatePicker, mYear: Int, mMonth: Int, mDayOfMonth: Int ->
-            beginDatum.value = "$mDayOfMonth/${mMonth+1}/$mYear"
-        }, startYear, startMonth, startDay
-    )
-
-    val endDatePickerDialog = DatePickerDialog(
-        mContext,
-        { _: DatePicker, mYear: Int, mMonth: Int, mDayOfMonth: Int ->
-            eindDatum.value = "$mDayOfMonth/${mMonth+1}/$mYear"
-        }, endYear, endMonth, endDay
+            datum.value = "$mDayOfMonth/${mMonth+1}/$mYear"
+        }, mYear, mMonth, mDay
     )
 
     Column (
         modifier = Modifier.fillMaxWidth(),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(
-            text = "Datum",
-            textAlign = TextAlign.Center,
-            fontSize = 20.sp
-        )
-        Spacer(modifier = Modifier.height(40.dp))
-        Text(text = "Geselecteerde start datum: ${beginDatum.value}", fontSize = 20.sp, textAlign = TextAlign.Center)
+        Text(text = "Geselecteerde ${naamDatum}: ${datum.value}", fontSize = 20.sp, textAlign = TextAlign.Center)
         Spacer(modifier = Modifier.height(10.dp))
         Button(onClick = {
-            startDatePickerDialog.show()
+            mDatePickerDialog.show()
         }, colors = ButtonDefaults.buttonColors(containerColor = Color(android.graphics.Color.parseColor("#C8A86E"))) ) {
 
-            Text(text = "Open Date Picker", color = Color.White)
+            Text(text = "Open Datum Picker", color = Color.White)
         }
-        Spacer(modifier = Modifier.size(20.dp))
-        Text(text = "Geselecteerde eind datum: ${eindDatum.value}", fontSize = 20.sp, textAlign = TextAlign.Center)
-        Spacer(modifier = Modifier.height(10.dp))
-        Button(onClick = {
-            endDatePickerDialog.show()
-        }, colors = ButtonDefaults.buttonColors(containerColor = Color(android.graphics.Color.parseColor("#C8A86E"))) ) {
 
-            Text(text = "Open Date Picker", color = Color.White)
-        }
 
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TijdPicker (label:String,value:String,onChange:(String)->Unit) {
+
+    OutlinedTextField(
+        label = { Text(text = label, color = Color(android.graphics.Color.parseColor("#C8A86E"))) },
+        value = value,
+        onValueChange =onChange,
+        modifier = Modifier
+            .fillMaxWidth()
+            .defaultMinSize(minHeight = 75.dp)
+            .height(75.dp)
+            .padding(horizontal = 40.dp),
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            focusedBorderColor = Color(android.graphics.Color.parseColor("#C8A86E")),
+            unfocusedBorderColor = Color(android.graphics.Color.parseColor("#C8A86E"))
+        ),
+        //trailingIcon = Icon(Icons.Default.Clear,contentDescription = null),
+    )
+
+
 }
