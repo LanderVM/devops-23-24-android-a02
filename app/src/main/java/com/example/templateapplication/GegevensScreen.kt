e86e3048fc7a8a796966f6b439b63e7a1b03a7ef
SanderGeuens <sander.geuens@student.hogent.be>
2023-10-15 15:24:01 +0200

    toevoegen van state aan gegevens scherm

--- app/src/main/java/com/example/templateapplication/GegevensScreen.kt
+++ app/src/main/java/com/example/templateapplication/GegevensScreen.kt
@@ -23,6 +23,10 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
@@ -32,12 +36,34 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import org.intellij.lang.annotations.RegExp
 
 
 @Composable
 fun GegevensScreen (modifier: Modifier = Modifier) {
 
     val scrollState = rememberScrollState()
+
+    var naam by remember { mutableStateOf("") }
+    var voornaam by remember { mutableStateOf("") }
+    var typeEvenement by remember { mutableStateOf("") }
+    var email by remember { mutableStateOf("") }
+
+    var straat by remember { mutableStateOf("") }
+    var huisnummer by remember { mutableStateOf("") }
+    var gemeente by remember { mutableStateOf("") }
+    var postcode by remember { mutableStateOf("") }
+
+    val buttonEnabled:Boolean
+    if (naam.isBlank()||naam.isEmpty()||voornaam.isBlank()||voornaam.isEmpty()||typeEvenement.isBlank()||
+        typeEvenement.isEmpty()||email.isBlank()||email.isEmpty()||
+        straat.isBlank()||straat.isEmpty()||huisnummer.isBlank()||huisnummer.isEmpty()||
+        gemeente.isBlank()||gemeente.isEmpty()||postcode.isBlank()||postcode.isEmpty()) {
+        buttonEnabled = false
+    } else {
+        buttonEnabled = true
+    }
+
 
     Column (
         modifier = Modifier
@@ -46,9 +72,19 @@ fun GegevensScreen (modifier: Modifier = Modifier) {
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Spacer(modifier = Modifier.height(20.dp))
-        ContactGegevens()
+        ContactGegevens(
+            naam = naam ,onNaamChange = {naam = it},
+            voornaam = voornaam, onVoornaamChange = {voornaam = it},
+            typeEvenement = typeEvenement, onTypeEvenementChange = {typeEvenement = it},
+            email = email, onEmailChange = {email = it}
+        )
         Spacer(modifier = Modifier.height(20.dp))
-        Adressering()
+        Adressering(
+            straat = straat, onStraatChange = {straat = it},
+            huisnummer = huisnummer, onHuisnummerChange = {huisnummer = it},
+            gemeente = gemeente, onGemeenteChange = {gemeente = it},
+            postcode = postcode, onPostcodeChange = {postcode = it},
+        )
         Spacer(modifier = Modifier.height(15.dp))
         Button (
             onClick = {},
@@ -59,6 +95,7 @@ fun GegevensScreen (modifier: Modifier = Modifier) {
                 contentColor = Color(android.graphics.Color.parseColor(stringResource(id = R.string.wit))),
                 disabledContentColor = Color(android.graphics.Color.parseColor(stringResource(id = R.string.wit)))
             ),
+            enabled = buttonEnabled
         ) {
             Text (text= "volgende")
         }
@@ -69,38 +106,50 @@ fun GegevensScreen (modifier: Modifier = Modifier) {
 
 
 @Composable
-fun ContactGegevens(modifier: Modifier = Modifier) {
+fun ContactGegevens(
+    modifier: Modifier = Modifier,
+    naam:String,onNaamChange:(String)->Unit,
+    voornaam:String,onVoornaamChange:(String)->Unit,
+    typeEvenement:String,onTypeEvenementChange:(String)->Unit,
+    email:String,onEmailChange:(String)->Unit,
+) {
     Column (
         modifier = Modifier.fillMaxWidth(),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(text = "Contact gegevens",textAlign = TextAlign.Center,fontSize = 20.sp)
         Spacer(modifier = Modifier.height(20.dp))
-        InputVeld(label="naam")
+        InputVeld(label="naam", value = naam, onChange = onNaamChange)
         Spacer(modifier = Modifier.height(20.dp))
-        InputVeld(label = "voornaam")
+        InputVeld(label = "voornaam",value=voornaam, onChange = onVoornaamChange)
         Spacer(modifier = Modifier.height(20.dp))
-        InputVeld(label = "Type Evenement")
+        InputVeld(label = "Type Evenement",value = typeEvenement, onChange = onTypeEvenementChange)
         Spacer(modifier = Modifier.height(20.dp))
-        InputVeld(label = "Email")
+        InputVeld(label = "Email", value = email, onChange = onEmailChange)
     }
 }
 
 @Composable
-fun Adressering() {
+fun Adressering(
+    modifier: Modifier = Modifier,
+    straat:String,onStraatChange:(String)->Unit,
+    huisnummer:String,onHuisnummerChange:(String)->Unit,
+    gemeente:String,onGemeenteChange:(String)->Unit,
+    postcode:String,onPostcodeChange:(String)->Unit,
+) {
     Column (
         modifier = Modifier.fillMaxWidth(),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(text = "Adress gegevens",textAlign = TextAlign.Center,fontSize = 20.sp)
         Spacer(modifier = Modifier.height(20.dp))
-        InputVeld(label="straat")
+        InputVeld(label="straat", value = straat, onChange = onStraatChange)
         Spacer(modifier = Modifier.height(20.dp))
-        InputVeld(label = "huisnummer")
+        InputVeld(label = "huisnummer", value = huisnummer, onChange = onHuisnummerChange)
         Spacer(modifier = Modifier.height(20.dp))
-        InputVeld(label = "gemeente")
+        InputVeld(label = "gemeente", value = gemeente, onChange = onGemeenteChange)
         Spacer(modifier = Modifier.height(20.dp))
-        InputVeld(label = "postcode")
+        InputVeld(label = "postcode", value = postcode, onChange = onPostcodeChange)
         Spacer(modifier = Modifier.height(15.dp))
         OptieFacturatieAdress()
     }
@@ -109,12 +158,14 @@ fun Adressering() {
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun InputVeld(modifier: Modifier = Modifier,label:String) {
+fun InputVeld(
+    modifier: Modifier = Modifier,label:String,value: String,onChange:(String)->Unit
+) {
 
     OutlinedTextField(
         label = { Text(text = label, color = Color(android.graphics.Color.parseColor(stringResource(id = R.string.lichter)))) },
-        value = "",
-        onValueChange = {},
+        value = value,
+        onValueChange =onChange,
         modifier = Modifier
             .fillMaxWidth()
             .defaultMinSize(minHeight = 75.dp)
@@ -148,10 +199,10 @@ fun OptieFacturatieAdress (modifier:Modifier = Modifier) {
     }
 }
 
-@Preview(showBackground = true, backgroundColor = 0xFFF5F0EE)
-@Composable
-fun FavoriteCollectionCardPreview() {
-    InputVeld(label = "naam")
-}
+//@Preview(showBackground = true, backgroundColor = 0xFFF5F0EE)
+//@Composable
+//fun FavoriteCollectionCardPreview() {
+//    InputVeld(label = "naam")
+//}
 
 
