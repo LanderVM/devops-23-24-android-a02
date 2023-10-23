a1d5a5c8109c3ac90feb1107fd7f9de0380ee9ba
Thomas Lissens <thomas.lissens@student.hogent.be>
2023-10-23 14:44:19 +0200

    small card update

--- app/src/main/java/com/example/templateapplication/ui/screens/formulepage/components/DetailsFormuleCard.kt
+++ app/src/main/java/com/example/templateapplication/ui/screens/formulepage/components/DetailsFormuleCard.kt
@@ -13,6 +13,7 @@ import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -27,7 +28,7 @@ fun DetailsFormuleCard(
     textColor: Color = Color.Black,
 ) {
     Card(
-        // shape = RectangleShape,
+        shape = RectangleShape,
         colors = CardDefaults.cardColors(
             containerColor = backgroundColor,
         ),
