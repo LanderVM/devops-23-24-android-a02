cb340c99580732392fac92941009bf50f0132142
Thomas Lissens <thomas.lissens@student.hogent.be>
2023-10-27 11:00:42 +0200

    Update navigation drawer

--- app/src/main/java/com/example/templateapplication/navigation/navidrawer/NavigationDrawer.kt
+++ app/src/main/java/com/example/templateapplication/navigation/navidrawer/NavigationDrawer.kt
@@ -1,25 +1,35 @@
 package com.example.templateapplication.navigation.navidrawer
 
 import android.content.Context
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Divider
 import androidx.compose.material3.DrawerState
+import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.NavigationDrawerItemDefaults
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 import com.example.templateapplication.navigation.NavigationRoutes
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
@@ -77,11 +87,17 @@ fun NavigationDrawer(
                     selectedContainerColor = Color.Transparent
                 ),
                 label = {
-                    Text(
-                        text = item.title.uppercase(),
-                        fontSize = 40.sp,
-                        overflow = TextOverflow.Ellipsis
-                    )
+                    Box(
+                       contentAlignment = Alignment.CenterStart
+                    ) {
+                        Text(
+                            text = item.title.uppercase(),
+                            maxLines = 2,
+                            fontSize = 30.sp,
+                            lineHeight = 35.sp,
+                            overflow = TextOverflow.Ellipsis,
+                            )
+                    }
                 },
                 selected = index == selectedItemIndex,
                 onClick = {
@@ -92,9 +108,26 @@ fun NavigationDrawer(
                     }
                 },
                 modifier = Modifier
+                    .height(80.dp)
                     .padding(NavigationDrawerItemDefaults.ItemPadding)
             )
         }
     }
 }
 
+@Preview
+@Composable
+fun NavigationDrawerPreview() {
+    val navController = rememberNavController()
+    val selectedItemIndex = 1 // Replace with your desired selected item index
+    val drawerState = rememberDrawerState(DrawerValue.Open)
+    val context = LocalContext.current
+
+    NavigationDrawer(
+        navController = navController,
+        selectedItemIndex = selectedItemIndex,
+        scope = rememberCoroutineScope(),
+        drawerState = drawerState,
+        context = context
+    )
+}
