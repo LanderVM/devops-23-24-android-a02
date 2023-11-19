a0f7fe1be5d0a6281f3d8f19e3e8bca56bbbd14c
Maurice Cantaert <maurice.cantaert@student.hogent.be>
2023-11-19 16:10:18 +0100

    fix selected date in guide price estimation not being reused in quotation offer

--- app/src/main/java/com/example/templateapplication/ui/screens/guideprice/GuidePriceScreen.kt
+++ app/src/main/java/com/example/templateapplication/ui/screens/guideprice/GuidePriceScreen.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.rememberDateRangePickerState
-import androidx.compose.material3.rememberTimePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -17,15 +16,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.templateapplication.data.Datasource
 import com.example.templateapplication.model.formules.FormuleViewModel
-import com.example.templateapplication.ui.commons.AutoCompleteComponent
 import com.example.templateapplication.ui.commons.DatumPart
-import com.example.templateapplication.ui.commons.ProgressieBar
-import com.example.templateapplication.ui.commons.VolgendeKnop
-import com.example.templateapplication.ui.screens.evenementpage.TimePart
 import java.util.Calendar
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -35,10 +29,14 @@ fun GuidePriceScreen(
     formulaViewModel: FormuleViewModel = viewModel()
 ) {
     val formulasList = Datasource().loadFormules()
-    val formuleUiState by formulaViewModel.formuleUiState.collectAsState()
+    val formulaUIState by formulaViewModel.formuleUiState.collectAsState()
+    val selectedStartDate = remember { mutableStateOf(formulaUIState.beginDatum) }
+    val selectedEndDate = remember { mutableStateOf(formulaUIState.eindDatum) }
     val scrollState = rememberScrollState()
 
-    val datumState = rememberDateRangePickerState(
+    val dataState = rememberDateRangePickerState(
+        initialSelectedStartDateMillis = selectedStartDate.value?.timeInMillis,
+        initialSelectedEndDateMillis = selectedEndDate.value?.timeInMillis,
         yearRange = IntRange(start = 2023, endInclusive = Calendar.getInstance().get(Calendar.YEAR) + 1),
     )
 
@@ -49,7 +47,7 @@ fun GuidePriceScreen(
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
         DatumPart(
-            state = datumState,
+            state = dataState,
             formulaViewModel = formulaViewModel,
         )
         Spacer(modifier = Modifier.height(35.dp))
