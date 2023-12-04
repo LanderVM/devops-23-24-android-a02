d0865b88ee3a6407173a2f961253053c5313e5cd
LanderVM <lander.vanmolle@student.hogent.be>
2023-12-04 14:09:30 +0100

    added floating action button

--- app/src/main/java/com/example/templateapplication/ui/screens/homepage/HomeScreen.kt
+++ app/src/main/java/com/example/templateapplication/ui/screens/homepage/HomeScreen.kt
@@ -1,12 +1,21 @@
 package com.example.templateapplication.ui.screens.homepage
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.example.templateapplication.R
 import com.example.templateapplication.model.home.HomeViewModel
 import com.example.templateapplication.ui.commons.AlertPopUp
@@ -25,43 +34,36 @@ fun HomeScreen(
     homeViewModel: HomeViewModel
 ) {
     val homeUiState by homeViewModel.homeUiState.collectAsState()
-
-    when {
-        homeUiState.openDialog -> {
-            AlertPopUp(
-                onDismissRequest = { homeViewModel.updateOpenDialog(false)  },
-                onConfirmation = {
-                    homeViewModel.updateOpenDialog(false)
-                    onGuidePriceNavigation()
-                },
-                dialogTitle = stringResource(R.string.alertPopUp_title),
-                dialogText = stringResource(R.string.alertPopUp_text),
-                confirmText = stringResource(R.string.alertPopUp_confirm),
-                dismissText = stringResource(R.string.alertPopUp_dismiss)
-            )
-        }
-    }
-
     val image1 = painterResource(id = R.drawable.foto4)
     val image2 = painterResource(id = R.drawable.foto5)
     val image3 = painterResource(id = R.drawable.foto8)
     val image4 = painterResource(id = R.drawable.foto7)
 
-    LazyColumn {
-        item {
-            HomeScreenTop(openDrawer = openDrawer)
-        }
-        item {
-            FormuleCard(title = stringResource(id = R.string.formula_basic_title), image = image1, onButtonClicked = onBasicNavigation)
-        }
-        item {
-            FormuleCard(title =stringResource(id = R.string.formula_allIn_title), image = image2, onButtonClicked = onAllInNavigation)
-        }
-        item {
-            FormuleCard(title =stringResource(id = R.string.formula_advanced_title), image = image3, onButtonClicked = onGevorderedNavigation)
-        }
-        item {
-            FormuleCard(title =stringResource(id = R.string.formula_extraMaterials_title), image = image4, onButtonClicked = onExtraNavigation)
+    Scaffold(
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {
+                    homeViewModel.updateOpenDialog(false)
+                    onGuidePriceNavigation()
+                },
+                content = {
+                    // Adjust the icon or text for FAB as needed
+                    Icon(Icons.Filled.Info, contentDescription = "Localized description")
+                },
+                // Position the FAB at the bottom right
+                modifier = Modifier.padding(all = 16.dp)
+            )
+        },
+        floatingActionButtonPosition = FabPosition.End
+    ) {
+        it
+        LazyColumn(modifier) {
+            item { HomeScreenTop(openDrawer = openDrawer) }
+            item { FormuleCard(title = stringResource(id = R.string.formula_basic_title), image = image1, onButtonClicked = onBasicNavigation) }
+            item { FormuleCard(title = stringResource(id = R.string.formula_allIn_title), image = image2, onButtonClicked = onAllInNavigation) }
+            item { FormuleCard(title = stringResource(id = R.string.formula_advanced_title), image = image3, onButtonClicked = onGevorderedNavigation) }
+            item { FormuleCard(title = stringResource(id = R.string.formula_extraMaterials_title), image = image4, onButtonClicked = onExtraNavigation) }
         }
     }
 }
+
